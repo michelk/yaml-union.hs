eb55164caff7d40951066c67073d84f73c39412e
Piotr Bogdan <ppbogdan@gmail.com>
2016-12-16 15:59:22 +0000

    Array unions.

--- src/Data/Yaml/Union.hs
+++ src/Data/Yaml/Union.hs
@@ -8,6 +8,8 @@ module Data.Yaml.Union
 import           Data.ByteString (ByteString)
 import qualified Data.HashMap.Strict as M
 import           Data.Maybe (catMaybes,mapMaybe)
+import           Data.Vector (Vector)
+import qualified Data.Vector as Vec
 import           Data.Yaml
 
 decodeBytestrings
@@ -42,5 +44,29 @@ union = M.unionWith dispatch
 
 dispatch :: Value -> Value -> Value
 dispatch (Object v1) (Object v2) = Object (v1 `union` v2)
---dispatch (Array v1) (Array v2) = undefined
+dispatch (Array v1) (Array v2) = Array $ vecUnion v1 v2
 dispatch _ x   = x
+
+vecUnion
+  :: Eq a
+  => Vector a -> Vector a -> Vector a
+vecUnion = vecUnionBy (==)
+
+vecUnionBy :: (a -> a -> Bool) -> Vector a -> Vector a -> Vector a
+vecUnionBy eq xs ys =
+  (Vec.++) xs (foldl (flip (vecDeleteBy eq)) (vecNubBy eq ys) xs)
+
+vecDeleteBy :: (a -> a -> Bool) -> a -> Vector a -> Vector a
+vecDeleteBy eq x ys
+  | Vec.length ys == 0 = ys
+  | otherwise =
+    if x `eq` Vec.head ys
+      then (Vec.tail ys)
+      else Vec.head ys `Vec.cons` vecDeleteBy eq x (Vec.tail ys)
+
+vecNubBy :: (a -> a -> Bool) -> Vector a -> Vector a
+vecNubBy eq vec
+  | Vec.length vec == 0 = vec
+  | otherwise =
+    Vec.head vec `Vec.cons`
+    vecNubBy eq (Vec.filter (not . eq (Vec.head vec)) (Vec.tail vec))
